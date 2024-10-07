be9b9227ae3ba7d7948d57e327fb1db8d433d961
Leonardo Dario Loaiza Sighuas <leonardodario.loaiza@unmsm.edu.pe>
2024-10-06 21:24:33 -0500

    Update aula.py
    nuevos estados para las aulas

--- domain/entities/aula.py
+++ domain/entities/aula.py
@@ -3,12 +3,25 @@ from typing import List, Optional
 
 class Aula(BaseModel):
     id: int
+    tipo: str # Tipo de aula (ej. Normal, Magna, laboratorio)
     capacidad: int
-    recursos: List[str]  # Lista de recursos disponibles en el aula
-    disponible: bool = True
+    recursos: List[str]
+    estado: str = "available" # Estado del aula (ej. disponible, en mantenimiento, ocupada, reservada)
 
     def is_available(self) -> bool:
-        return self.disponible
+        return self.estado == "available"
+    
+    def marcar_reservada(self) -> bool:
+        return self.estado == "reserved"
+    
+    def marcar_ocupada(self) -> bool:
+        return self.estado == "occupied"
+    
+    def marcar_disponible(self) -> bool:
+        return self.estado == "available"
+    
+    def marcar_mantenimiento(self) -> bool:
+        return self.estado == "in maintenance"
 
     def can_accommodate(self, numero_de_estudiantes: int) -> bool:
         return numero_de_estudiantes <= self.capacidad
